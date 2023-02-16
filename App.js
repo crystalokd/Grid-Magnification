fc778c261189fee272f7876ab7e0208801c2bf74
Crystal <crystalokd@gmail.com>
2023-02-16 19:14:59 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import { Canvas } from '@shopify/react-native-skia';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window')
+const SQUARES_AMOUNT_HORIZONTAL = 10
+const SQUARES_CONTAINER_SIZE = SCREEN_WIDTH / SQUARES_AMOUNT_HORIZONTAL
+const PADDING = 10
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Canvas style={{ flex: 1, backgroundColor: 'red'}}>
+
+      </Canvas>
     </View>
   );
 }
@@ -14,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
